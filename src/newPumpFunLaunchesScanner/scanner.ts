ed5f978ac9f507ac5caee422993b71de2d206ecc
Nikola Bozin <mail@rizary.com>
2024-12-27 10:41:55 +0100

    feat: add pumpfun new launches scanner

---
+++ src/newPumpFunLaunchesScanner/scanner.ts
@@ -0,0 +1,16 @@
+import WebSocket from 'ws';
+
+const ws = new WebSocket('wss://pumpportal.fun/api/data');
+
+ws.on('open', function open() {
+
+  let payload = {
+      method: "subscribeNewToken", 
+    }
+  ws.send(JSON.stringify(payload));
+
+});
+
+ws.on('message', function message(data:any) {
+  console.log(JSON.parse(data));
+});
